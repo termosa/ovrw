161d2ae37d21ca389e65024df8d818a44a3968d6
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-11-23 22:39:04 +0200

    Fix deps and platform specific difference

--- bin/ovrw.js
+++ bin/ovrw.js
@@ -8,7 +8,7 @@ new Liftoff({
     name: 'ovrw',
     processTitle: 'ovrw',
 }).launch({}, (env, argv) => {
-    const lastBinIndex = argv.findIndex(arg => arg.endsWith('ovrw'))
+    const lastBinIndex = argv.findIndex(arg => arg.match(/ovrw(\.js)?$/))
     const { _: [ src ], dist, port } = minimist(argv.slice(lastBinIndex < 0 ? 0 : lastBinIndex + 1), { alias: { d: 'dist', p: 'port' } })
     const cwd = process.cwd()
     ovrw(src ? path.resolve(cwd, src) : cwd, {
